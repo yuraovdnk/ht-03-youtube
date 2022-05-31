423cb195328d2fd5d306bbd4ece30bff10d9bdb1
yuraovdnk <yuraovdnk1999@ukr.net>
2022-05-31 15:57:41 +0300

    fix mongo uri

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -12,8 +12,7 @@ export type postsType = {
     content:string
     bloggerId:number
 }
-
-const mongoUri = process.env.MongoURI || "mongodb://localhost:27017"
+const mongoUri = process.env.MongoURI || 'mongodb+srv://yuraovdnk:ovdey1970@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(mongoUri)
 const db = client.db('youtube')
 export const bloggerCollection = db.collection<bloggerType>("bloggers")
